f5acbe6b667042ba43a8fffe01fb2aa238e6c008
黄辉来 <1175215747@qq.com>
2023-04-17 18:13:06 +0800

    fix：接口调试之后改动

--- bosen-gateway/src/main/java/com/bosen/gateway/authorization/AuthorizationManager.java
+++ bosen-gateway/src/main/java/com/bosen/gateway/authorization/AuthorizationManager.java
@@ -39,10 +39,6 @@ public class AuthorizationManager implements ReactiveAuthorizationManager<Author
 
     @Override
     public Mono<AuthorizationDecision> check(Mono<Authentication> mono, AuthorizationContext authorizationContext) {
-        // 测试情况下跳过鉴权
-        if (true) {
-            return Mono.just(new AuthorizationDecision(true));
-        }
         ServerHttpRequest request = authorizationContext.getExchange().getRequest();
         URI uri = request.getURI();
         PathMatcher pathMatcher = new AntPathMatcher();
