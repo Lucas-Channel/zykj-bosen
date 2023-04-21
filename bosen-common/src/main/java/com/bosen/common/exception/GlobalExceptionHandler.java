87bfc913a6880e8caad99c821cc71336149877d1
黄辉来 <1175215747@qq.com>
2023-04-21 10:35:54 +0800

    fix 添加e.printStackTrace();

--- bosen-common/src/main/java/com/bosen/common/exception/GlobalExceptionHandler.java
+++ bosen-common/src/main/java/com/bosen/common/exception/GlobalExceptionHandler.java
@@ -12,6 +12,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = BusinessException.class)
     public ResponseData<Void> handleBusinessException(BusinessException e) {
+        e.printStackTrace();
         if (e.getCode() != null) {
             return ResponseData.fail(e.getCode().getCode(), e.getMessage());
         }
@@ -20,6 +21,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
     public ResponseData<Void> handleException(Exception e) {
+        e.printStackTrace();
         return ResponseData.fail(e.getMessage());
     }
 
