c1a821c869883b05715f2986a0199f01ee80a5a2
黄辉来 <1175215747@qq.com>
2023-06-05 13:07:40 +0800

    redis 白名单数据，set

---
+++ bosen-common/src/main/java/com/bosen/common/constant/common/RedisDataBaseIndexConstants.java
@@ -0,0 +1,85 @@
+package com.bosen.common.constant.common;
+
+/**
+ * redis database 下标常量类
+ */
+public class RedisDataBaseIndexConstants {
+
+    public static final Integer DEFAULT_INDEX = 0;
+
+    /**
+     * redis数据库下标值（用户库）
+     */
+    public static final Integer REDIS_USER_INDEX = 0;
+
+    /**
+     * redis数据库下标值（商品库）
+     */
+    public static final Integer REDIS_PRODUCT_INDEX = 1;
+
+    /**
+     * redis数据库下标值（订单库）
+     */
+    public static final Integer REDIS_ORDER_INDEX = 2;
+
+    /**
+     * redis数据库下标值（后台基础库）
+     */
+    public static final Integer REDIS_PLATFORM_MANAGE_INDEX = 3;
+
+    /**
+     * redis数据库下标值（短信服务库）
+     */
+    public static final Integer REDIS_SMS_INDEX = 4;
+
+    /**
+     * redis数据库下标值（报表服务库）
+     */
+    public static final Integer REDIS_REPORT_INDEX = 5;
+
+    /**
+     * redis数据库下标值（支付服务库）
+     */
+    public static final Integer REDIS_PAY_INDEX = 6;
+
+    /**
+     * redis数据库下标值（采购服务库）
+     */
+    public static final Integer PURCHASE_INDEX = 7;
+
+    /**
+     * redis数据库下标值（工作流服务库）
+     */
+    public static final Integer REDIS_WORKFLOW_INDEX = 8;
+
+    /**
+     * redis数据库下标值（合同服务库）
+     */
+    public static final Integer REDIS_CONTRACT_INDEX = 9;
+
+    /**
+     * redis数据库下标值（模板服务库）
+     */
+    public static final Integer REDIS_TEMPLATE_INDEX = 10;
+
+    /**
+     * redis数据库下标值（营销服务库）
+     */
+    public static final Integer REDIS_MARKETING_INDEX = 11;
+
+    /**
+     * redis数据库下标值（OpenAPI服务库）
+     */
+    public static final Integer REDIS_OPEN_API_INDEX = 12;
+
+    /**
+     * redis数据库下标值（结算服务）
+     */
+    public static final Integer REDIS_SETTLEMENT_INDEX = 13;
+
+    /**
+     * redis数据库下标值（搜索服务）
+     */
+    public static final Integer REDIS_SEARCH_INDEX = 14 ;
+
+}
