0fad035c0cfd364d56f1b3681ada12aab4df61ff
huanghuilai <1175215747@qq.com>
2023-08-02 14:54:27 +0800

    标签

---
+++ bosen-system/src/main/java/com/bosen/system/service/ILabelService.java
@@ -0,0 +1,9 @@
+package com.bosen.system.service;
+
+/**
+ * @author Lucas
+ * @version 2.0.0
+ * @date 2023/8/2
+ */
+public interface ILabelService {
+}
---
+++ bosen-system/src/main/java/com/bosen/system/service/impl/LabelServiceImpl.java
@@ -0,0 +1,13 @@
+package com.bosen.system.service.impl;
+
+import com.bosen.system.service.ILabelService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Lucas
+ * @version 2.0.0
+ * @date 2023/8/2
+ */
+@Service
+public class LabelServiceImpl implements ILabelService {
+}
